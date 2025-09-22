b530669e2766e69cc5102b811bc4c8baaada142c
luiz.sena <luiz.sena@ufam.edu.br>
2025-09-22 06:43:10 -0400

    feat(parser): adicionar schemas e expandir método parse_data
    - Criadas classes Pydantic para representar o schema de produtos e suas relações.
    - Expandido o método `parse_data` para suportar parsing de título, grupo, rank de vendas, categorias, reviews e produtos similares.

--- src/parser/parser.py
+++ src/parser/parser.py
@@ -1,5 +1,8 @@
 import re
 
+from schema import Product, Group
+
+
 class AmazonParser:
     """
     Parser para dados do Amazon
@@ -55,21 +58,69 @@ class AmazonParser:
     def parse_data(lines: list[str]):
         id_pattern = re.compile(r'^Id:\s*(\d+)$')
         asin_pattern = re.compile(r'^ASIN:\s*(\w+)$')
+        title_pattern = re.compile(r'^ {2}title:\s*(.+)$')
+        group_pattern = re.compile(r'^ {2}group:\s*(.+)$')
+        sales_rank_pattern = re.compile(r'^ {2}salesrank:\s*(\d+)$')
+        category_pattern = re.compile(r'^ {2}categories:\s*(.+)$')
+        review_pattern = re.compile(r'^ {2}reviews:\s*(.+)$')
+        similar_pattern = re.compile(r'^ {2}similar:\s*(.+)$')
 
-        if match := id_pattern.match(lines[0]):
-            product_id = match.group(1)
-            print(f"Product ID: {product_id}")
+        # Modelo de Product para setar as variáveis
+        product = Product(
+            id_product="",
+            asin="",
+        )
 
-        if match := asin_pattern.match(lines[0]):
-            product_asin = match.group(1)
-            print(f"Product ID: {product_id}")
+        for line in lines:
+            if match := id_pattern.match(line):
+                product_id = match.group(1)
+                product.id_product = product_id
+                continue
 
+            if match := asin_pattern.match(line):
+                product_asin = match.group(1)
+                product.asin = product_asin
+                continue
+
+            if match := title_pattern.match(line):
+                product_title = match.group(1)
+                product.title = product_title
+                continue
+
+            if match := group_pattern.match(line):
+                product_group = match.group(1)
+                product.group = Group(name=product_group)
+                print(f"Product Group: {product.group}")
+                continue
+
+            if match := sales_rank_pattern.match(line):
+                product_sales_rank = match.group(1)
+                product.salesrank = product_sales_rank
+                print(f"Product Sales Rank: {product_sales_rank}")
+                continue
+
+            if match := category_pattern.match(line):
+                product_category = match.group(1)
+                print(f"Product Category: {product_category}")
+                continue
+
+            if match := review_pattern.match(line):
+                product_review = match.group(1)
+                print(f"Product Review: {product_review}")
+                continue
+
+            if match := similar_pattern.match(line):
+                product_similar = match.group(1)
+                print(f"Product Similar: {product_similar}")
+                continue
+
+        print(product)
         print(lines)
 
 
 parser = AmazonParser("data/amazon-meta.txt")
 
-cur_line = 3
+cur_line = 7
 
 lines = parser.get_data(cur_line)[0]
 
