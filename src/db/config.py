b825bfe98976774b052c54d5db420c57ac1a54d1
luiz.sena <luiz.sena@ufam.edu.br>
2025-09-24 16:04:50 -0400

    refactor(database): ajustar lógica de conexão para permitir configuração dinâmica
    - Refatorada classe `DatabaseConnection` para aceitar configurações dinâmicas de conexão.
    - Modificada classe `Config` para suportar inicialização com argumentos opcionais.
    - Melhorado gerenciamento de variáveis de ambiente.

--- src/db/config.py
+++ src/db/config.py
@@ -3,12 +3,26 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+
 class Config:
-    DB_HOST : str = os.getenv("DB_HOST")
-    DB_PORT : str = os.getenv("DB_PORT")
-    DB_NAME : str = os.getenv("DB_NAME")
-    DB_USER : str = os.getenv("DB_USER")
-    DB_PASSWORD : str = os.getenv("DB_PASSWORD")
-    if not all([DB_HOST, DB_PORT, DB_NAME, DB_USER, DB_PASSWORD]): 
-        raise EnvironmentError("Erro ao setar variáveis de ambiente.\n"
-                               "Verifique se em .env há os campos: DB_HOST, DB_PORT, DB_NAME, DB_USER, DB_PASSWORD")
+    DB_HOST: str = os.getenv("DB_HOST")
+    DB_PORT: str = os.getenv("DB_PORT")
+    DB_NAME: str = os.getenv("DB_NAME")
+    DB_USER: str = os.getenv("DB_USER")
+    DB_PASSWORD: str = os.getenv("DB_PASSWORD")
+
+    def __init__(self,
+                 db_host=None,
+                 db_port=None,
+                 db_name=None,
+                 db_user=None,
+                 db_password=None):
+        self.DB_HOST = db_host or self.DB_HOST
+        self.DB_PORT = db_port or self.DB_PORT
+        self.DB_NAME = db_name or self.DB_NAME
+        self.DB_USER = db_user or self.DB_USER
+        self.DB_PASSWORD = db_password or self.DB_PASSWORD
+
+        if not all([self.DB_HOST, self.DB_PORT, self.DB_NAME, self.DB_USER, self.DB_PASSWORD]):
+            raise EnvironmentError("Erro ao setar variáveis de ambiente.\n"
+                                   "Verifique se em .env há os campos: DB_HOST, DB_PORT, DB_NAME, DB_USER, DB_PASSWORD")
--- src/db/connection.py
+++ src/db/connection.py
@@ -2,22 +2,26 @@ import psycopg2 as pg
 from psycopg2.pool import SimpleConnectionPool
 from db.config import Config
 
-_pool = SimpleConnectionPool(
-    minconn=1,
-    maxconn=10,
-    user=Config.DB_USER,
-    password=Config.DB_PASSWORD,
-    host=Config.DB_HOST,
-    port=Config.DB_PORT,
-    database=Config.DB_NAME
-)
 
 class DatabaseConnection:
-    def __init__(self):
+    _pool: SimpleConnectionPool = None
+
+    def __init__(self, db_host=None, db_port=None, db_name=None, db_user=None, db_password=None):
+        config = Config(db_host, db_port, db_name, db_user, db_password)
+
+        self._pool = SimpleConnectionPool(
+            minconn=1,
+            maxconn=10,
+            user=config.DB_USER,
+            password=config.DB_PASSWORD,
+            host=config.DB_HOST,
+            port=config.DB_PORT,
+            database=config.DB_NAME
+        )
         self._conn_stack: list[pg.extensions.connection] = []
 
     def __enter__(self) -> pg.extensions.connection:
-        conn = _pool.getconn()
+        conn = self._pool.getconn()
         self._conn_stack.append(conn)
         return conn
 
@@ -32,4 +36,4 @@ class DatabaseConnection:
                 else:
                     conn.rollback()
             finally:
-                _pool.putconn(conn)
+                self._pool.putconn(conn)
